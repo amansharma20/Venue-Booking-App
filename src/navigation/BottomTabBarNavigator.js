2ae62b3e421d06431acb8742f90fed6f945b7a88
Aman <amanacts20@gmail.com>
2021-09-30 18:15:12 +0530

    Bottom Tab Navigator code cleaned

--- src/navigation/BottomTabBarNavigator.js
+++ src/navigation/BottomTabBarNavigator.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
-import { Image, Platform, TouchableOpacity, View } from 'react-native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image, TouchableOpacity, View } from 'react-native';
 import HomeScreen from '../screens/home/HomeScreen';
 import Icons from '../../constants/Icons';
-import { FONTS, icons } from '../../constants';
-import LinearGradient from "react-native-linear-gradient";
+import { FONTS } from '../../constants';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 
-// const Tab = createBottomTabNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
 export default function BottomTabBarNavigator(props, focused) {
@@ -25,13 +22,11 @@ export default function BottomTabBarNavigator(props, focused) {
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused }) => {
                     const tintColor = focused ? '#EC6A93' : '#ffffff';
-                    // const backgroundColor = focused ? '#3511A3' : '#DECEE9';
                     switch (route.name) {
                         case 'Home':
                             return (
                                 <View
                                     style={{
-                                        // backgroundColor: backgroundColor,
                                         width: '100%',
                                         height: '100%',
                                         justifyContent: 'center',
@@ -43,7 +38,6 @@ export default function BottomTabBarNavigator(props, focused) {
                                         source={Icons.homeIcon}
                                         resizeMode="contain"
                                         style={{
-                                        // backgroundColor: backgroundColor,
                                             tintColor: tintColor,
                                             width: 24,
                                             height: 24,
@@ -55,7 +49,6 @@ export default function BottomTabBarNavigator(props, focused) {
                             return (
                                 <View
                                     style={{
-                                        // backgroundColor: backgroundColor,
                                         width: '100%',
                                         height: '100%',
                                         justifyContent: 'center',
@@ -78,7 +71,6 @@ export default function BottomTabBarNavigator(props, focused) {
                                 <TouchableOpacity onPress={() => setTabBarShowLabel(!tabBarShowLabel)}>
                                     <View
                                         style={{
-                                            // backgroundColor: backgroundColor,
                                             flex: 1,
                                             justifyContent: 'center',
                                             alignItems: 'center',
@@ -99,28 +91,8 @@ export default function BottomTabBarNavigator(props, focused) {
                     }
                 },
                 headerShown: false,
-                // tabBarShowLabel: tabBarShowLabel,
-                // tabBarLabelPosition: beside-icon,
                 tabBarLabelStyle: { fontSize: 12, fontFamily: FONTS.satoshi700 },
                 tabBarInactiveBackgroundColor: 'red',
-                // tabBarActiveTintColor: '#EC6A93',
-                // tabBarInactiveBackgroundColor: '#172047',
-                // tabBarActiveBackgroundColor: '#393E61',
-                // tabBarBackground: () => (
-                //     <LinearGradient
-                //         style={{flex: 1}}
-                //         colors={[
-                //             "#454C6B",
-                //             "#2D3058"
-                //         ]}
-                //     ></LinearGradient>
-                // ),
-                // tabBarStyle: {
-                //     height: Platform.select({
-                //         ios: 80,
-                //         android: 50,
-                //     }),
-                // },
             })}>
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Schedule" component={HomeScreen} />
