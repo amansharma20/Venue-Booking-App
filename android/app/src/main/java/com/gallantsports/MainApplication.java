afa774ad1cd248668c89decfecbe1a0cf2e6c8bd
Aman <amanacts20@gmail.com>
2021-10-31 01:08:22 +0530

    local changes

--- android/app/src/main/java/com/gallantsports/MainApplication.java
+++ android/app/src/main/java/com/gallantsports/MainApplication.java
@@ -41,12 +41,12 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-  }
+  // @Override
+  // public void onCreate() {
+  //   super.onCreate();
+  //   SoLoader.init(this, /* native exopackage */ false);
+  //   initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  // }
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
@@ -55,27 +55,28 @@ public class MainApplication extends Application implements ReactApplication {
    * @param context
    * @param reactInstanceManager
    */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.gallantsports.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
+   
+  // private static void initializeFlipper(
+  //     Context context, ReactInstanceManager reactInstanceManager) {
+  //   if (BuildConfig.DEBUG) {
+  //     try {
+  //       /*
+  //        We use reflection here to pick up the class that initializes Flipper,
+  //       since Flipper library is not available in release mode
+  //       */
+  //       Class<?> aClass = Class.forName("com.gallantsports.ReactNativeFlipper");
+  //       aClass
+  //           .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+  //           .invoke(null, context, reactInstanceManager);
+  //     } catch (ClassNotFoundException e) {
+  //       e.printStackTrace();
+  //     } catch (NoSuchMethodException e) {
+  //       e.printStackTrace();
+  //     } catch (IllegalAccessException e) {
+  //       e.printStackTrace();
+  //     } catch (InvocationTargetException e) {
+  //       e.printStackTrace();
+  //     }
+  //   }
+  // }
 }
